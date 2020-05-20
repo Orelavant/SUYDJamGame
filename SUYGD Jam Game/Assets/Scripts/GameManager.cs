880329de60323637cc88b353356b2b2832c5aa34
Jacob Valero <54868391+Orelavant@users.noreply.github.com>
2020-05-20 14:58:54 -0600

    Small changes

--- SUYGD Jam Game/Assets/Scripts/GameManager.cs
+++ SUYGD Jam Game/Assets/Scripts/GameManager.cs
@@ -7,7 +7,7 @@ using TMPro;
 public class GameManager : MonoBehaviour
 {
     // References
-    public string[] colors;
+    public string[] textColors;
     public TextMeshProUGUI orderText;
 
     // Modulate for harder game
@@ -16,10 +16,36 @@ public class GameManager : MonoBehaviour
     // Keep track of orders
     public List<List<string>> orders = new List<List<string>>();
 
+    //Colors
+    private static Color blue = new Color(48 / 255f, 96 / 255f, 130 / 255f);
+    private static Color purple = new Color(118 / 255f, 66 / 255f, 138 / 255f);
+    private static Color red = new Color(172 / 255f, 50 / 255f, 50 / 255f);
+    private static Color orange = new Color(223 / 255f, 113 / 255f, 38 / 255f);
+    private static Color yellow = new Color(251 / 255f, 242 / 255f, 54 / 255f);
+    private static Color green = new Color(106 / 255f, 190 / 255f, 48 / 255f);
+    private static Color pink = new Color(215 / 255f, 123 / 255f, 186 / 255f);
+    private static Color black = new Color(0f, 0f, 0f);
+    private static Color brown = new Color(102 / 255f, 57 / 255f, 49 / 255f);
+    public Hashtable colorHash = new Hashtable();
+
+    //Paint Storage sprite
+    public GameObject paintStorageIcon;
+
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("Orders", 2, orderDelay);
+
+        // Colors to hashtable.
+        colorHash.Add("blue", blue);
+        colorHash.Add("purple", purple);
+        colorHash.Add("red", red);
+        colorHash.Add("orange", orange);
+        colorHash.Add("yellow", yellow);
+        colorHash.Add("green", green);
+        colorHash.Add("pink", pink);
+        colorHash.Add("black", black);
+        colorHash.Add("brown", brown);
     }
 
     private void Orders() {
@@ -31,12 +57,12 @@ public class GameManager : MonoBehaviour
         // Sloppy, change if possible.
         List<string> currOrder = new List<string>();
         for (int i = 0; i < 3; i++) {
-            int randIndex = Random.Range(0, colors.Length);
+            int randIndex = Random.Range(0, textColors.Length);
             while (exclude.Contains(randIndex)) {
-                randIndex = Random.Range(0, colors.Length);
+                randIndex = Random.Range(0, textColors.Length);
             }
             exclude.Add(randIndex);
-            currOrder.Add(colors[randIndex]);
+            currOrder.Add(textColors[randIndex]);
         }
 
         orders.Add(currOrder);
--- SUYGD Jam Game/Assets/Scripts/PlayerController.cs
+++ SUYGD Jam Game/Assets/Scripts/PlayerController.cs
@@ -26,21 +26,7 @@ public class PlayerController : MonoBehaviour
     List<string> colorStorage = new List<string>();
     public GameObject[] paintStorage;
     private string currColor;
-
-    //Colors
-    private static Color blue = new Color(48 / 255f, 96 / 255f, 130 / 255f);
-    private static Color purple = new Color(118 / 255f, 66 / 255f, 138 / 255f);
-    private static Color red = new Color(172 / 255f, 50 / 255f, 50 / 255f);
-    private static Color orange = new Color(223 / 255f, 113 / 255f, 38 / 255f);
-    private static Color yellow = new Color(251 / 255f, 242 / 255f, 54 / 255f);
-    private static Color green = new Color(106 / 255f, 190 / 255f, 48 / 255f);
-    private static Color pink = new Color(215 / 255f, 123 / 255f, 186 / 255f);
-    private static Color black = new Color(0f, 0f, 0f);
-    private static Color brown = new Color(102 / 255f, 57 / 255f, 49 / 255f);
-    private static Color grey = new Color(173 / 255f, 173 / 255f, 173 / 255f);
-    private static Color white = new Color(255 / 255f, 255 / 255f, 255 / 255f);
-    private Hashtable colorHash = new Hashtable();
-
+    public static Color white = new Color(255 / 255f, 255 / 255f, 255 / 255f);
 
     //Booleans
     private bool crazyIsActive = true;
@@ -50,17 +36,6 @@ public class PlayerController : MonoBehaviour
     private void Start() {
         gameManagerScript = eventSystem.GetComponent<GameManager>();
         rb = GetComponent<Rigidbody2D>();
-
-        // Colors
-        colorHash.Add("blue", blue);
-        colorHash.Add("purple", purple);
-        colorHash.Add("red", red);
-        colorHash.Add("orange", orange);
-        colorHash.Add("yellow", yellow);
-        colorHash.Add("green", green);
-        colorHash.Add("pink", pink);
-        colorHash.Add("black", black);
-        colorHash.Add("brown", brown);
     }
 
     // Update is called once per frame
@@ -133,7 +108,7 @@ public class PlayerController : MonoBehaviour
         //Update color storage and text.
         if (colorStorage.Count < 3) {
             colorStorage.Add(color);
-            paintStorage[colorStorage.Count - 1].GetComponent<Renderer>().material.color = (Color)colorHash[color];
+            paintStorage[colorStorage.Count - 1].GetComponent<Renderer>().material.color = (Color)gameManagerScript.colorHash[color];
             playerColorsText.text += " " + color;
         }
     }
